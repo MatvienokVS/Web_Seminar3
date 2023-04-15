1cf7eacadda1f28a6cb2c252894d8c7ebe8f2454
MatvienokVS <skavaava@gmail.com>
2023-04-16 00:48:40 +0300

    решение задач web_seminar 3

---
+++ script/main.js
@@ -0,0 +1,20 @@
+function allDoc(){
+  alert("Журнал документов пустой");
+}
+
+function addDoc(){
+  alert("Документ успешно создан");
+}
+
+function searchDoc(){
+  alert("Документ не найден");
+}
+
+function controlDoc(){
+  alert("Все документы обработаны");
+}
+
+function sendMessage(){
+    var InputValue = document.getElementById('name').value;
+    alert("Данные отправлены");
+}
